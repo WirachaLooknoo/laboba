cff56dc802ed7501b9d32660a34df7feb892a7bf
WirachaLooknoo <looknoo@Macintosh.local>
2021-06-16 20:25:04 +0700

    add logo

--- navbar.php
+++ navbar.php
@@ -6,8 +6,12 @@
 </head>
 
 <body>
+    
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
+        <a class="item1">
+        <img class="logo" src="Untitled_Artwork 3.PNG">
+    </a>
             <a class="navbar-brand" href="../laboba/index.php">LABOBA</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
